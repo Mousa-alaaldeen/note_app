bd617d427ef50da97f78522ce4076827678ac7c8
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-17 13:55:43 +0300

    custom app bar

---
+++ lib/view/widget/note_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+import 'custom_app_bar.dart';
+
+class NoteViewBody extends StatelessWidget {
+  const NoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 18),
+      child: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
