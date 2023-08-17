cccbbdd810b09f7c99957177be0feb1a8f86ebc2
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-17 20:38:42 +0300

    create edit note view

--- lib/view/widget/custom_note_itm.dart
+++ lib/view/widget/custom_note_itm.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'edit_note_view_body.dart';
+
 class NoteItm extends StatelessWidget {
   const NoteItm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.deepOrange,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              child: Text(
-                'Build your career with Mousa',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black.withOpacity(0.4),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteViewBody();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                child: Text(
+                  'Build your career with Mousa',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.4),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May21,2023',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21,2023',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.4),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view/widget/edit_note_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'edit_note_view_body.dart';
+
+class EdetNoteView extends StatelessWidget {
+  const EdetNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const EditNoteViewBody();
+  }
+}
