3b0f568acb6d81ed78a69a115c72a9ec6146e00a
alaaldee <musa.alaaldeen77@gmail.com>
2023-08-17 16:57:58 +0300

    create bottm sheet

--- lib/view/note_view.dart
+++ lib/view/note_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note/view/widget/add_note_bottm_sheet.dart';
 import 'package:note/view/widget/note_view_body.dart';
 
 class NoteView extends StatelessWidget {
@@ -11,7 +12,13 @@ class NoteView extends StatelessWidget {
         child: NoteViewBody(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottmSheet();
+              });
+        },
         child: const Icon(Icons.add),
       ),
     );
